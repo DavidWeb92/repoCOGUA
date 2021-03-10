01e5beceaf945f2724c8818c84c945cf028ceb0c
DavidWeb92 <david818quin@gmail.com>
2021-03-10 16:46:59 -0500

    archivos estaticos

--- Turismo/settings.py
+++ Turismo/settings.py
@@ -164,15 +164,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-
-    os.path.join(PROJECT_DIR,'/static/'),
-
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ##lineas de codigo para las imagenes
 MEDIA_URL = '/media/'
